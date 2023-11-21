2b7927701dfa798311b74f8b87efe52c791fe719
BenvonBa <benedikt.balint@sevdesk.de>
2023-11-21 11:01:53 +0100

    fixed missing images
    fixed missing images

--- src/templates/sportangebote.js
+++ src/templates/sportangebote.js
@@ -11,20 +11,20 @@ export default function Sportangebote(props) {
     <Layout>
       {" "}
       <SportContentSection
-        title={pageContext.title}
-        description={pageContext.content}
-        imageURL={pageContext.image.url}
+        title={pageContext?.title}
+        description={pageContext?.content}
+        imageURL={pageContext?.image?.url}
       />
       <div className="gallerySpacing">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <ImageGallery imageArray={pageContext.imageGallery} />{" "}
+          <ImageGallery imageArray={pageContext?.imageGallery} />{" "}
         </div>
       </div>
     </Layout>
   )
 }
 export const Head = props => {
-  const seoSettings = (props && props.pageContext.seoSettings) || {}
+  const seoSettings = (props && props.pageContext?.seoSettings) || {}
   const pageData = props && props.pageContext
 
   return (
